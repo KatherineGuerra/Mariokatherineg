aceaaf572f1ac41993a206f2df2f15685396ef67
katherine guerra <guerrakn@.gmail>
2014-11-10 12:13:29 -0800

    commit

--- public_html/Mario Files/js/resources.js
+++ public_html/Mario Files/js/resources.js
@@ -4,7 +4,7 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-
+        {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -15,7 +15,7 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-
+        {name: "kathylevel01", type: "tmx", src: "data/map/kathylevel01.tmx"}
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
