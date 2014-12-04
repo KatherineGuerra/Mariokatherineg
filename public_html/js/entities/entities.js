fdaff6e5785863d18de4ca9b8b4728011052fba1
katherine guerra <guerrakn@.gmail>
2014-12-04 11:28:04 -0800

    commit

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -30,21 +30,18 @@ game.PlayerEntity = me.Entity.extend ({
             this.flipX(false);
             this.body.vel.x += this.body.accel.x * me.timer.tick;
 
+        } else if (me.input.isKeyPressed("left")) {
+            this.flipX(true);
+            this.body.vel.x -= this.body.accel.x * me.timer.tick;
+
         } else {
             this.body.vel.x = 0;
         }
 
-        this.body.update(delta);
-        me.collision.check(this, true, this.collideHandler.bind(this), true);
+ 
 
-        if (this.body.vel.x !== 0) {
-            if (!this.renderable.isCurrentAnimation("smallWalk")) {
-                this.renderable.setCurrentAnimation("smallWalk");
-                this.renderable.setAnimationFrame();
-            }
-        } else {
-            this.renderable.setCurrentAnimation("idle");
-        }
+
+
 
 
         if (me.input.isKeyPressed("up")) {
@@ -56,13 +53,7 @@ game.PlayerEntity = me.Entity.extend ({
         }
 
 
-        if (me.input.isKeyPressed("left")) {
-            this.flipX(true);
-            this.body.vel.x -= this.body.accel.x * me.timer.tick;
-
-        } else {
-            this.body.vel.x = 0;
-        }
+       
 
         this.body.update(delta);
         me.collision.check(this, true, this.collideHandler.bind(this), true);
@@ -100,6 +91,42 @@ game.LevelTrigger = me.Entity.extend({
        me.levelDirector.loadLevel(this.level);
        me.state.current().resetPlayer(this.xSpawn, this.ySpawn);
     }
+});
+
+game.BadGuy = me.Entity.extend({
+    init: function(x, y, settings){
+        this._super(me.Entity, 'init', [x, y, {
+                image: "slime",
+                spritewidth: "60",
+                spriteheight: "28",
+                width: 60,
+                height: 28,
+                getShape: function(){
+                    return (new me.Rect(0, 0, 60, 28)).toPolygon();
+                }
+        }]);
+    
+    this.spritewidth = 60;
+    var width = settings.width;
+    x = this.pos.x;
+    this.startX = x;
+    this.endX = x + width - this.spritewidth;
+    this.pos.x = x + width - this.spritewidth;
+    this.updateBounds();
+    
+    this.alwaysUpdate = true;
+    
+    this.walkLeft = false;
+    this.Alive = true;
+    this.type = "BadGuy";
+    
+    this.renderable.addAnimation("run", [0, 1, 2], 80);
+    
+    },
+    update: function(delta){
+        
+    }
+    
 });
  
         
